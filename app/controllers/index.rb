c93359cab8fc60e277cb9c57d20cbd8ac51cf5df
Apprentice <apprentice@dbc-chicago-009.local>
2013-10-03 16:25:31 -0500

    added images and bootstrapping header

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,20 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+
+post '/shuffle' do
+  erb :index, layout: !request.xhr? 
+  # If the user passes-in a "value", let's use it. Otherwise, we'll generate a random one.
+
+  # See: roll_if_value_is_nil method in the Roll model.
+  value = params[:value] ? params[:value].to_i : nil
+
+  @roll = value ? Roll.create({ value: value }) : Roll.create
+
+  if request.xhr?
+    erb :_die_roll, layout: false
+  else
+  erb :_shuffle   # HINT: what does this do? what should we do instead?
+end 
+end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('img#1').submit(function(event){
+    event.preventDefault();
+    var url = $(this).attr(action)
+    var data = { value : Math.floor(Math.random() * 10 +1 }
+      $.post(url, data, function(response) {
+        console.log(response);
+        $('#shuffle').html(response);
+      });
+  })
 });
