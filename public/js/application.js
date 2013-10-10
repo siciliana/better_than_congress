54221321def9dd50c102bf4cd6dead7b265aacc1
siciliana <siciliana@gmail.com>
2013-10-09 22:59:02 -0500

    cleaning up and fixing photo positioning

--- public/js/application.js
+++ public/js/application.js
@@ -1,36 +1,21 @@
 $(function(){
     (function(){
-        var imgs=$('#congress img');
-        var imgs2=$('#personage img');
+        var imgs=$('#personage img');
+        // var imgs2=$('#personage img');
         var l=imgs.length-1, M=Math,
         i=M.floor((M.random()*(l+1))), current=i;
 
         function shuffle(){
-            $(imgs[i]).fadeIn(100, function(){
+            $(imgs[i]).fadeIn(1000, function(){
                 i=M.floor((M.random()*(l+1)));
                 i=(current==i)?((i+1)>l?(i-1):(i+1)):i;
                 current=i;
                 setTimeout(function(){
-                    $('#congress img').fadeOut(100);
+                    $('#personage img').fadeOut(1000);
                     shuffle();
-                }, 100);
+                }, 1000);
             });
-
-            $(imgs2[i]).fadeIn(100, function(){
-                i=M.floor((M.random()*(l+1)));
-                i=(current==i)?((i+1)>l?(i-1):(i+1)):i;
-                current=i;
-                setTimeout(function(){
-                    $('#personage img').fadeOut(100);
-                    shuffle();
-                }, 100);
-            });
-
         }
         shuffle();
     })();
-
-
-
-
 });
