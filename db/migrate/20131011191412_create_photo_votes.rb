f91ebab5764f96a3fe03fd7837365d44194ba12e
siciliana <siciliana@gmail.com>
2013-10-22 12:17:54 -0500

    Procfile

--- db/migrate/20131011191412_create_photo_votes.rb
+++
@@ -1,9 +0,0 @@
- class CreatePhotoVotes < ActiveRecord::Migration
-  def change
-    create_table :photo_votes do |t|
-      t.integer :photo_id
-      t.ingeger :vote_id
-      t.timestamps
-    end
-  end
-end
